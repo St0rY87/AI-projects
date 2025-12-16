2d3e8f5fd35f97705aa2e59ecd8bebc9aaf08a6c
Dmitriy <mr.dima337@gmail.com>
2025-12-16 11:57:10 +0300

    feat: update vite config

--- chatBot/packages/server/index.ts
+++ chatBot/packages/server/index.ts
@@ -2,15 +2,19 @@ import express from "express";
 import type { Request, Response } from "express";
 import dotenv from "dotenv";
 
-dotenv.config();
+dotenv.config(); //Загружает переменные окружения
 
-const app = express();
+const app = express(); //Создает экземпляр приложения Express
 const port = process.env.PORT || 3000;
 
-app.use(express.json());
+app.use(express.json()); //Подключает middleware для автоматического парсинга JSON из тела запроса
 
 app.get("/", (req: Request, res: Response) => {
-  res.send('Hello, World!');
+  res.send('Hello, World!'); // При GET-запросе на / возвращает текст "Hello, World!"
+});
+
+app.get("/api/hello", (req: Request, res: Response) => {
+  res.json({message: 'Hello, World!'});
 });
 
 app.listen(port, () => {
